442d589169e91bc2fc82b31ed18c3ee855b1bea8
Jonathan Feenstra <26406078+JonathanFeenstra@users.noreply.github.com>
2025-02-02 15:42:39 +0100

    Fix softlock when talking to an NPC that's bleeding out

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2023 Jonathan Feenstra
+Copyright (C) 2023, 2025 Jonathan Feenstra
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,33 +14,104 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-class DialogueMenuEx final : public RE::DialogueMenu
+class SkipTextFunctionHandler final : public RE::GFxFunctionHandler
+{
+public:
+	static void Install(const RE::DialogueMenu* a_dialogueMenu)
+	{
+		RE::GFxValue dialogueMenu_mc;
+		if (a_dialogueMenu->uiMovie->GetVariable(&dialogueMenu_mc, "_root.DialogueMenu_mc")) {
+			auto handler = RE::make_gptr<SkipTextFunctionHandler>();
+			RE::GFxValue skipTextOriginal;
+			dialogueMenu_mc.GetMember("SkipText", &skipTextOriginal);
+			dialogueMenu_mc.SetMember("SkipTextOriginal", skipTextOriginal);
+
+			RE::GFxValue skipTextNew;
+			a_dialogueMenu->uiMovie->CreateFunction(&skipTextNew, handler.get());
+			dialogueMenu_mc.SetMember("SkipText", skipTextNew);
+		}
+	}
+
+	void Call(Params& a_params) override
+	{
+		RE::GFxValue topicList;
+		if (!a_params.thisPtr->GetMember("TopicList", &topicList))
+			return;
+
+		RE::GFxValue entriesA;
+		if (!topicList.GetMember("EntriesA", &entriesA))
+			return;
+
+		if (entriesA.GetArraySize() == 0) {
+			// this can happen when talking to an NPC that's bleeding out (e.g. after a brawl), preventing any dialogue progress except for skipping/cancelling
+			a_params.thisPtr->Invoke("SkipTextOriginal", nullptr, a_params.args, a_params.argCount);
+		}
+	}
+};
+
+class OnCancelPressFunctionHandler final : public RE::GFxFunctionHandler
+{
+public:
+	static void Install(const RE::DialogueMenu* a_dialogueMenu)
+	{
+		RE::GFxValue dialogueMenu_mc;
+		if (a_dialogueMenu->uiMovie->GetVariable(&dialogueMenu_mc, "_root.DialogueMenu_mc")) {
+			auto handler = RE::make_gptr<OnCancelPressFunctionHandler>();
+			RE::GFxValue onCancelPressNew;
+			a_dialogueMenu->uiMovie->CreateFunction(&onCancelPressNew, handler.get());
+			dialogueMenu_mc.SetMember("onCancelPress", onCancelPressNew);
+		}
+	}
+
+	void Call(Params& a_params) override
+	{
+		a_params.thisPtr->Invoke("StartHideMenu", nullptr, nullptr, 0);
+	}
+};
+
+class MenuOpenCloseEventSink final : public RE::BSTEventSink<RE::MenuOpenCloseEvent>
 {
 public:
 	static void Install()
 	{
-		REL::Relocation<uintptr_t> vtbl(RE::VTABLE_DialogueMenu[0]);
-		_AcceptFn = vtbl.write_vfunc(0x1, &AcceptEx);
+		const auto singleton = GetSingleton();
+		if (const auto ui = RE::UI::GetSingleton()) {
+			ui->AddEventSink(singleton);
+		}
 	}
 
-	void AcceptEx(CallbackProcessor* a_cbReg)
+	static MenuOpenCloseEventSink* GetSingleton()
 	{
-		_AcceptFn(this, a_cbReg);
-		fxDelegate->callbacks.Remove("SkipText");
-		a_cbReg->Process("SkipText", [](const RE::FxDelegateArgs&) {
-			// Empty
-		});
+		static MenuOpenCloseEventSink singleton;
+		return &singleton;
 	}
+
+	RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent* a_event, RE::BSTEventSource<RE::MenuOpenCloseEvent>*) override
+	{
+		if (a_event->menuName == RE::DialogueMenu::MENU_NAME && a_event->opening) {
+			const auto dialogueMenu = RE::UI::GetSingleton()->GetMenu<RE::DialogueMenu>().get();
+			if (dialogueMenu) {
+				SkipTextFunctionHandler::Install(dialogueMenu);
+				OnCancelPressFunctionHandler::Install(dialogueMenu);
+			}
+		}
+
+		return RE::BSEventNotifyControl::kContinue;
+	}
+
+	MenuOpenCloseEventSink() = default;
 
 private:
-	using AcceptFn = decltype(&DialogueMenu::Accept);
-
-	inline static REL::Relocation<AcceptFn> _AcceptFn;
+	MenuOpenCloseEventSink(const MenuOpenCloseEventSink&) = delete;
+	MenuOpenCloseEventSink(MenuOpenCloseEventSink&&) = delete;
+	~MenuOpenCloseEventSink() = default;
+	MenuOpenCloseEventSink& operator=(const MenuOpenCloseEventSink&) = delete;
+	MenuOpenCloseEventSink& operator=(MenuOpenCloseEventSink&&) = delete;
 };
 
 SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
 	SKSE::Init(skse);
-	DialogueMenuEx::Install();
+	MenuOpenCloseEventSink::Install();
 	return true;
 }
